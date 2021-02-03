8b667f3c757bb97f0428486244bb130457bca105
Takaforyannick30 <takaforyannick30@gmail.com>
2021-02-03 02:44:36 +0000

    Added meals urls in root urls.py

--- foodium/urls.py
+++ foodium/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
+    path('meals/', include('meals.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
