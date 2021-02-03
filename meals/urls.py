28fffb1bfbd7933fe778a15d56408cfab8e221ed
Takaforyannick30 <takaforyannick30@gmail.com>
2021-02-03 22:42:51 +0000

    Added url path for meal_detail function in urls.py in meals app

--- meals/urls.py
+++ meals/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_meals, name='meals')
+    path('', views.all_meals, name='meals'),
+    path('<meal_id>', views.meal_detail, name='meal_detail'),
 ]
