69bb79600fdf4b49a1b0225e1531b5576a864d33
Takaforyannick30 <takaforyannick30@gmail.com>
2021-02-03 02:17:18 +0000

    Added CategoryAdmin and MealAdmin list display in the admin.py of meals app

--- meals/admin.py
+++ meals/admin.py
@@ -3,5 +3,29 @@ from .models import Category, Meal
 
 # Register your models here.
 
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'slug'
+    )
+
+
+class MealAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'slug',
+        'category',
+        'price',
+        'description'
+        'people'
+        'preparation_time'
+        'rating',
+        'image',
+        'stock',
+        'available'
+    )
+
+
 admin.site.register(Category)
 admin.site.register(Meal)
