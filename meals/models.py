cae29d38560d202e54071531500c3adef63a39e7
Takaforyannick30 <takaforyannick30@gmail.com>
2021-02-03 00:32:10 +0000

    Created category model in meals app

--- meals/models.py
+++ meals/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=254, unique=True)
+    slug = models.SlugField(max_length=254, unique=True)
+
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
