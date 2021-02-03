4c40aab3259c16de7b28fc0de9b1d7b1ddad8ef2
Takaforyannick30 <takaforyannick30@gmail.com>
2021-02-03 02:28:34 +0000

    Created basic all_meals function in views.py in meals app

--- meals/views.py
+++ meals/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Meal
 
 # Create your views here
+
+
+def all_meals(request):
+    """ A view to display all meals, including seach queries """
+
+    meals = Meal.objects.all()
+
+    context = {
+        'meals': meals,
+    }
+
+    return render(request, meals/meals.html, context)
