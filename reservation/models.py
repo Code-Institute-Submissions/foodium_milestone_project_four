846e34a2d785a16110268d636a7a6d2b85fc26b9
Takaforyannick30 <takaforyannick30@gmail.com>
2021-02-04 23:41:45 +0000

    Created Reservation model in the models.py file of reservation app

--- reservation/models.py
+++ reservation/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Reservation(models.Model):
+    '''
+     Fields guests will need to fill to make a reservation
+    '''
+    name = models.CharField(max_length=100)
+    phone = models.IntegerField()
+    email = models.EmailField()
+    number_of_persons = models.IntegerField()
+    date = models.DateField()
+    time = models.TimeField()
+
+    def __str__(self):
+        return self.name
