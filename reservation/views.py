2d763296c9d526c160f9cf70018f6f7bc26ca708
Takaforyannick30 <takaforyannick30@gmail.com>
2021-02-05 00:18:20 +0000

    Created reserve_a_table view function in reservation app

--- reservation/views.py
+++ reservation/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Reservation
 
 # Create your views here.
+
+
+def reserve_a_table(request):
+
+    context = {}
+
+    return render(request, 'reservation/reservation.html', context)
