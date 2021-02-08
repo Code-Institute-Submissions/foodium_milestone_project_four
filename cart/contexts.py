4d663747d27fb27748ced417c2d6b48d9aac6244
Takaforyannick30 <takaforyannick30@gmail.com>
2021-02-08 18:43:32 +0000

    Updated contexts.py to update the cart when an item is added to it

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,4 +1,6 @@
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from meals.models import Meal
 
 
 def cart_contents(request):
@@ -6,6 +8,17 @@ def cart_contents(request):
     cart_items = []
     total = 0
     meal_count = 0
+    cart = request.session.get('cart', {})
+
+    for item_id, quantity in cart.items():
+        meal = get_object_or_404(Meal, pk=item_id)
+        total += quantity * meal.price
+        meal_count += quantity
+        cart_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'meal': meal,
+        })
 
     ''' Display delivery cost only if total is greater 0 '''
     if total > 0:
