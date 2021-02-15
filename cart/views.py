5b70b6ce5306db1dfaba130590913026d27b4d49
Takaforyannick30 <takaforyannick30@gmail.com>
2021-02-15 00:05:46 +0000

    Added ajust_cart view function urlpattern in cart app

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 
 # Create your views he
 
@@ -22,3 +22,21 @@ def add_to_cart(request, item_id):
 
     request.session['cart'] = cart
     return redirect(redirect_url)
+
+
+def adjust_cart(request, item_id):
+    """ Adjust the quantity of the specified item to the specified amount """
+
+    quantity = int(request.POST.get('quantity'))
+    cart = request.session.get('cart', {})
+    print(item_id)
+    print(quantity)
+    print(cart)
+
+    if quantity > 0:
+        cart[item_id] = quantity
+    else:
+        cart.pop(item_id)
+
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))
