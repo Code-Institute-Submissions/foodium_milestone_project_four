0b5f07fe20b9861f5d1852f88ae2282b350ebc45
Takaforyannick30 <takaforyannick30@gmail.com>
2021-02-20 19:16:39 +0000

    Updated handler404 function and added it to root urls.py

--- foodium/urls.py
+++ foodium/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
@@ -27,3 +28,5 @@ urlpatterns = [
     path('cart/', include('cart.urls')),
     path('checkout/', include('checkout.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = 'foodium.views.handler404'
--- foodium/views.py
+++ foodium/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render
 
 
-def handler404(request, exception, template_name="errors/error_404.html"):
+def handler404(request, exception):
     '''
     404 Error Handler
     '''
-    response = render, (template_name)
-    response.status_code = 404
-    return response
+    return render(request, 'errors/error_404.html')
