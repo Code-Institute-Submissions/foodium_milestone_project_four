82a17348f1d28725a6ece254495d9ae8e4623008
Takaforyannick30 <takaforyannick30@gmail.com>
2021-02-23 07:56:24 +0000

    Added profile view function and urls path

--- foodium/urls.py
+++ foodium/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
     path('reserve_a_table/', include('reservation.urls')),
     path('cart/', include('cart.urls')),
     path('checkout/', include('checkout.urls')),
+    path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 print(urlpatterns)
