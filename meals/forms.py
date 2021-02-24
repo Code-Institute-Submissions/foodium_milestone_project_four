66d75bc0481370813c74c3e01d22d091d5b2910e
Takaforyannick30 <takaforyannick30@gmail.com>
2021-02-24 15:12:21 +0000

    Added widget functionality to enable image display on admin panel

--- meals/forms.py
+++ meals/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .widgets import CustomClearableFileInput
 from .models import Meal, Category
 
 
@@ -7,6 +8,9 @@ class MealForm(forms.ModelForm):
     class Meta:
         model = Meal
         fields = '__all__'
+
+    image = forms.ImageField(label='Image', required=False,
+                             widget=CustomClearableFileInput)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
