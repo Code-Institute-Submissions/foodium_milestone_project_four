e8c6cc8917a6cd83a0fb0ac512517449bc7f2585
Takaforyannick30 <takaforyannick30@gmail.com>
2021-02-25 10:19:35 +0000

    Added contact view function and url path in contact app

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from .models import Contact
+from django.contrib import messages
+from .forms import ContactForm
+from django.core.mail import send_mail
 
-# Create your views here.
+import os
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+
+
+def contact(request):
+    """
+    A view to render contact form/template
+    """
+    if request.method == 'POST':
+        if request.user.is_authenticated:
+            create_contact_form = Contact(
+                name=request.POST.get('name'),
+                subject=request.POST.get('subject'),
+                email=request.POST.get('email'),
+                message=request.POST.get('message'),
+                user_id=request.user
+            )
+            create_contact_form.save()
+        else:
+            create_contact_form = Contact(
+                name=request.POST.get('name'),
+                subject=request.POST.get('subject'),
+                email=request.POST.get('email'),
+                comment=request.POST.get('comment'),
+            )
+            create_contact_form.save()
+
+        # Send Email
+        send_mail(
+            'New Form Submission',
+            'Hi Admin,\n\nYou have a new contact message. Sign '
+            'into the admin panel to view.\n\nRegards,'
+            'n\nFOODIUM',
+            'takaforyannick30@gmail.com',
+            [EMAIL_HOST_USER],
+            fail_silently=True
+        )
+
+        messages.success(request, 'Contact form successfully submitted.\
+                                    We will be in touch!')
+        return redirect(reverse('contact_success'))
+
+    context = {
+        'form': ContactForm
+    }
+    return render(request, 'contact/contact.html', context)
