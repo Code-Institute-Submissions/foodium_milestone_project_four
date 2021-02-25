237d2f28aaf769d4947928b6fc3e40ac314ffe62
Takaforyannick30 <takaforyannick30@gmail.com>
2021-02-25 09:58:20 +0000

    Registered Contact model in the admin.py file of contact app

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Contact
 
-# Register your models here.
+
+class ContactAdmin(admin.ModelAdmin):
+    # Fields to display
+    list_display = (
+        'name',
+        'subject',
+        'user_id',
+        'email',
+        'contact_date',
+    )
+
+    search_fields = (
+        'name',
+        'email',
+    )
+
+    list_per_page = 25
+
+    # orders contacts by date
+    ordering = ('contact_date',)
+
+
+admin.site.register(Contact, ContactAdmin)
