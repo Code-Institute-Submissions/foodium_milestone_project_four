ce533787b282b8f2ff9b68ed77931587e82954a3
Takaforyannick30 <takaforyannick30@gmail.com>
2021-02-26 01:39:24 +0000

    Added about_us view function and url path in pages app

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import faq_view
+from .views import faq_view, about_us
 
 
 urlpatterns = [
     path('faq/', faq_view, name="faq"),
+    path('about/', about_us, name="about_us"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -6,3 +6,10 @@ def faq_view(request, *args, **kwargs):
     A view to render frequently asked questions' template
     """
     return render(request, "pages/faq.html")
+
+
+def about_us(request, *args, **kwargs):
+    """
+    A view to render about_us template
+    """
+    return render(request, "pages/about_us.html")
