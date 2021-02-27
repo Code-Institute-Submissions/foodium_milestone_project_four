df3b8310373f54c43771b22ea82e0b00a61a004b
Takaforyannick30 <takaforyannick30@gmail.com>
2021-02-27 12:38:30 +0000

    Added contact_date and user_id to Reservation model in reservation app

--- reservation/models.py
+++ reservation/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-
-# Create your models here.
+from django.contrib.auth.models import User
+from datetime import datetime
 
 
 class Reservation(models.Model):
@@ -8,11 +8,13 @@ class Reservation(models.Model):
      Fields guests will need to fill to make a reservation
     '''
     name = models.CharField(max_length=100)
-    phone = models.IntegerField()
-    email = models.EmailField()
+    phone_number = models.IntegerField()
+    email = models.EmailField(max_length=100)
     number_of_persons = models.IntegerField()
     date = models.DateField()
     time = models.TimeField()
+    contact_date = models.DateTimeField(default=datetime.now)
+    user_id = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
